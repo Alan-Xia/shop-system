fb69c494ede3b3dfd8863c215af5ff1312d5af70
xiabenhui <237490540@qq.com>
2020-06-16 00:19:13 +0800

    添加进度条

--- src/plugins/request.js
+++ src/plugins/request.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 const serive = axios.create({
   baseURL: 'https://www.liulongbin.top:8888/api/private/v1/',
   timeout: 3000
 })
-
+// 在 request 拦截器中，展示进度条 NProgress.start()
 serive.interceptors.request.use(config => {
+  NProgress.start()
   let token = sessionStorage.getItem('access_token')
   if (token) {
     config.headers.Authorization = token
@@ -15,7 +18,9 @@ serive.interceptors.request.use(config => {
   return Promise.reject(error)
 })
 
+// 在 response 拦截器中，隐藏进度条 NProgress.done()
 serive.interceptors.response.use(response => {
+  NProgress.done()
   return response
 },error => {
   return Promise.reject(error)
